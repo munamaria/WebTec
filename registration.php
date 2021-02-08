dbdb552f80c6d34fec2cd4fd0bd4d34ef74547ce
munamaria <78038888+munamaria@users.noreply.github.com>
2021-02-07 23:51:46 -0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,29 @@
+<html>
+  
+   <h1 align="center"> registration</h1>
+    <body>
+        <form align="center">
+            name:
+            <input type="text" name="name"><br><br>
+            email:
+            <input type="text" name="email"><br><br>
+            username:
+            <input type="text" name="username"><br><br>
+            Password:
+            <input type="number" name="password"><br><br>
+            confirm pasword:
+            <input type="text" name="confirm password"><br><br>
+            Gender: <br>
+            <input type="radio" name="gender" value="male"> Male
+            <input type="radio" name="gender" value="female">Female<br><br>
+            <input type="radio" name="gender" value="other">other<br><br>
+            Date of Birth:
+            <input type="text" name="Date of Birth"><br><br>
+            
+            
+        
+            <input type="submit" value="submit">
+            
+        </form>
+    </body>
+</html>
